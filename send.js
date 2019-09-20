5f489fc606958068369d7a8a86ef08889e12d8ba
fowwo <54595444+fowwo@users.noreply.github.com>
2019-09-19 22:39:32 -0500

    Added basic CSS and htmls

---
+++ send.js
@@ -0,0 +1,14 @@
+function send(email){
+    var emailObject = {
+        email: email
+    }
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot){
+            success(); // some success method
+        }, function(error){
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+send(email);
