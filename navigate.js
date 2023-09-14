17fc2dda0cbe46eb6cda4ebdf717002d0de163cc
fowwo <54595444+fowwo@users.noreply.github.com>
2023-09-13 19:35:29 -0500

    Nest subtabs (2020.02.22)

--- navigate.js
+++ navigate.js
@@ -1,11 +1,30 @@
-var tabs = {
-	"index.html": "Play",
-	"leaderboard.html": "Leaderboard",
-	"#settings": "Settings"
-}
-var subtabs = {
-	"mastermind.html": "Mastermind"
-}
+var tabs = [
+	{
+		url: "index.html",
+		name: "Play",
+		subtabs: [
+			{
+				url: "mastermind.html",
+				name: "Mastermind"
+			}
+		]
+	},
+	{
+		url: "leaderboard.html",
+		name: "Leaderboard",
+		subtabs: [
+			{
+				url: "#mastermind",
+				name: "Mastermind"
+			}
+		]
+	},
+	{
+		url: "#settings",
+		name: "Settings",
+		subtabs: []
+	}
+]
 
 var div = document.createElement("div");
 var subdiv = document.createElement("div");
@@ -14,23 +33,24 @@ subdiv.className = "subtab";
 
 var url = window.location.pathname.split("/");
 url = url[url.length - 1];
-for (var key of Object.keys(tabs)) {
+
+for (var tab of tabs){
 	var a = document.createElement("a");
-	a.href = key;
-	a.innerHTML = tabs[key];
-    if (key == url){
+	a.href = tab.url;
+	a.innerHTML = tab.name;
+    if (tab.url == url){
 		a.className = "active";
+		for (var subtab of tab.subtabs){
+			var b = document.createElement("a");
+			b.href = subtab.url;
+			b.innerHTML = subtab.name;
+			if (subtab.url == url){
+				b.className = "active";
+			}
+			subdiv.appendChild(b);
+		}
 	}
 	div.appendChild(a);
-}
-for (var key of Object.keys(subtabs)) {
-	var a = document.createElement("a");
-	a.href = key;
-	a.innerHTML = subtabs[key];
-    if (key == url){
-		a.className = "active";
-	}
-	subdiv.appendChild(a);
 }
 document.body.appendChild(div);
 document.body.appendChild(subdiv);
